fb782919f936eaf29af72af179ca5110ced70a20
Zayy2005x <2781310592@qq.com>
2025-06-08 13:31:23 +0800

    阿里云OSS相关配置

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,4 +1,23 @@
 package com.sky.controller.admin;
 
+
+import com.sky.dto.DishDTO;
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "菜品相关接口")
+@Slf4j
+@RequestMapping("/admin/dish")
+@RestController
 public class DishController {
+
+    @PostMapping
+    public Result save(@RequestBody DishDTO dishDTO){
+
+    }
 }
